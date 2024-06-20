8ada3c575d6b814a1565280cdd1fb19a3cf3483c
Nishant dixit <145234347+bleedblack1@users.noreply.github.com>
2024-06-20 22:06:33 +0530

    Add files via upload
    day 20  of learnig java .

---
+++ STRING.java
@@ -0,0 +1,58 @@
+public class STRING {
+    
+    /*public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        // taking one word as input
+        String name = sc.next();
+
+        System.out.println(name);
+        // for taking one line as input
+        
+        String line = sc.nextLine();
+        System.out.println(line);
+
+    }
+
+    */
+
+    /* 
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        
+        // for taking one line as input
+        
+        String line = sc.nextLine();
+        System.out.println(line);
+
+    }
+    */
+
+
+    /* 
+    public static void main(String[] args) {
+      // find length of string  
+       String line =  " nishant dixit " ;
+        System.out.println(line.length());
+    }
+
+
+    */
+
+    //concatenation
+
+
+    public static void main(String[] args) {
+        // find length of string  
+         String firstname =  " nishant" ;
+         String lastname = "dixit ";
+          System.out.println(firstname + " "+lastname);
+      }
+
+
+
+
+
+}
