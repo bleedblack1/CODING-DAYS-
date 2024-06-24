68ad532bb44491cd96403121172ea178230b4be4
Nishant dixit <145234347+bleedblack1@users.noreply.github.com>
2024-06-24 22:44:55 +0530

    Add files via upload
    day 24 of learning java

---
+++ bitmanupulation.java
@@ -0,0 +1,20 @@
+public class bitmanupulation {
+    
+    public static void main(String[] args) {
+        //and operator
+        System.out.println((5&6));
+        //or operator
+        System.out.println((5|6));
+        // xor operator
+        System.out.println((5^6));
+         // one's compliment
+         System.out.println((~5));
+        // right shift
+        System.out.println((5>>1));
+        // left shift
+        System.out.println((5<<1));
+
+
+    }
+    
+}
