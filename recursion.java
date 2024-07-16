48b2f3e0ac2b35f03d4cc12927c28b2e23632e20
Nishant dixit <145234347+bleedblack1@users.noreply.github.com>
2024-07-16 23:27:40 +0530

    Add files via upload
    day 38 of learning java

---
+++ recursion.java
@@ -0,0 +1,17 @@
+public class recursion {
+
+    public static void printDec(int n) {
+        if (n == 1) {
+            System.out.println(n);
+            return;
+        } else {
+            System.out.println(n);
+            printDec(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        printDec(n);
+    }
+}
