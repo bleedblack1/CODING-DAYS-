99cc8ec2ecc41d0e105e5f15866fa8709e9e6ba8
Nishant dixit <145234347+bleedblack1@users.noreply.github.com>
2024-07-21 23:46:37 +0530

    Add files via upload
    day 43 of learning java

---
+++ recursionTest.java
@@ -0,0 +1,21 @@
+public class recursionTest {
+    
+    public static void printbinarystring(int n , int lastplace , String str ){
+        //base 
+        if(n == 0){
+            System.out.println(str);
+            return;
+        }
+        
+        
+        printbinarystring(n-1, 0, str+"0");
+        if (lastplace == 0) {
+            printbinarystring(n-1, 1, str+"1");
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        printbinarystring(3, 0, "");
+    }
+}
