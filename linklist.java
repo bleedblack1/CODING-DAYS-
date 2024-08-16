c631d24868cbcca37a18ed362a90e049750b700a
Nishant dixit <145234347+bleedblack1@users.noreply.github.com>
2024-08-16 23:02:41 +0530

    Add files via upload
    days 64

---
+++ linklist.java
@@ -0,0 +1,145 @@
+public class linklist {
+
+    public static class Node{
+        int data ;
+        Node next;
+
+        public Node(int data){
+            this.data = data ;
+            this.next = null ;
+        }
+    }
+    public static Node head ;
+    public static Node tail ;
+    public static int size = 0 ;
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            System.out.println("ll is empty");
+            head = new Node(data);
+            head = tail = newNode ;
+            return ;
+        }
+
+
+        //newNode next =head
+        newNode.next = head ;
+
+        //head = newNode
+        head = newNode ;
+
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = new Node(data);
+            head = tail = newNode ;
+            return ;
+        }
+        tail.next = newNode ;
+        tail=newNode;
+
+    }
+
+    public void add(int idx , int data){
+
+        if(idx==0){
+            addFirst(data);
+            return;
+        }
+        size++;
+
+        Node newNode = new Node(data);
+        Node temp = head ;
+        int i = 0 ;
+
+        while(i<idx-1){
+            temp = temp.next ;
+            i++ ;
+        }
+        newNode.next = temp.next ;
+        temp.next = newNode ;
+    }
+
+    public int removeFirst(){
+
+        if(size == 0 ){
+            System.out.println("ll is empty");
+            return Integer.MIN_VALUE;
+        }else if(size ==1){
+            int val = head.data ;
+            head = tail = null ;
+            size = 0 ;
+            return val;
+        }
+        int val = head.data;
+        head = head.next ;
+        size--;
+        return val ;
+    }
+
+    public int removeLast(){
+        if(size == 0 ){
+            System.out.println("ll is empty");
+            return Integer.MIN_VALUE;
+        }
+        else if(size ==1){
+            int val = head.data ;
+            head = tail = null ;
+            size = 0 ;
+            return val;
+        }
+
+        //previous node : i = size-2
+            Node prev = head;
+            for(int i=0; i<size-2 ; i++){
+                prev = prev.next ;
+
+            }
+            int val = prev.next.data; //tail.data
+            prev.next = null;
+            size--;
+            return val ;
+
+    }
+
+    
+
+
+
+    public void print(){
+
+        Node temp = head ;
+        while(temp != null){
+            System.out.print(temp.data + "--> ");
+            temp = temp.next ;
+        }
+        System.out.println();
+    }
+
+    
+    public static void main(String[] args) {
+        linklist ll = new linklist() ;
+        ll.addFirst(2);
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addFirst(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
+        ll.addLast(5);
+        ll.print();
+        ll.add(2, 7);
+        ll.print();
+        ll.removeFirst();
+        ll.print();
+        ll.removeLast();
+        ll.print();
+        System.out.println(size);
+    }
+}
